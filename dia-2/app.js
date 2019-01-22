c1a1bfc09bad27ba9d28c5012c751f5305be2d7f
davidCarba <da.carballal@gmail.com>
2019-01-22 21:40:43 +0100

    Dia-2

--- dia-2/app.js
+++ dia-2/app.js
@@ -4,3 +4,124 @@
  */
 
 console.log("Probando, probando, 1, 2, 3!");
+
+/*let counter = 1;
+let a = counter++;
+console.log(a);*/
+/*
+let x;
+console.log(x);*/
+
+/*
+console.log(typeof 0);
+console.log(typeof "pepe");
+console.log(typeof null);
+console.log(typeof Math);
+console.log(typeof alert);*/
+
+/*let value = true;
+console.log(typeof value);
+value = String(value);
+alert(value);
+let str = 123;
+console.log(typeof str);
+let num = Number(str);
+alert(num);
+alert(Boolean(1));
+alert(Boolean(0));
+alert(Boolean("pepe"));
+alert(Boolean(""));
+alert(Boolean(undefined));
+alert(Boolean(-1));*/
+
+/*let counter = 1;
+let a = counter++;
+console.log(a);*/
+
+//ejercicio 1
+
+/*let name = "Ilya";
+alert(`hello ${1}`); // number 1
+alert(`hello $("name")`); //string
+alert(`hello ${name}`); //variable name*/
+
+//ejercicio 2
+
+/* "" + 1 + 0; //"10"
+"" - 1 + 0; //1
+true + false; //1
+6 / "3"; //2
+"2" + "3"; //6
+4 + 5 + "px"; //"9px"
+"$" + 4 + 5; //"$45"
+"4" - 2; //2
+"4px" - 2; //NaN
+7 / 0; //infinity
+"  -9  " + 5; //"-9  5"
+"-9  " - 5; //-14
+null + 1; //1
+undefined + 1; //NaN */
+
+//ejercicio 3
+
+/* let a = 1;
+let b = 1;
+
+let c = ++a; //2
+let d = b++; //1
+a; //2
+b; //2 */
+
+//ejercicio 4
+
+/* let a = 2;
+let x = 1 + (a *= 2); //5
+a; //4 */
+
+//ejercicio 5
+
+/* 5 > 4; //true
+"apple" > "pineapple"; //false
+"2" > "12"; //true
+undefined == null; //true
+undefined === null; //false
+null == "\n0\n"; //false
+null === +"\n0\n"; //false
+10 == "10"; //true
+10 === "10"; //false */
+
+/*let age;
+age = 19;
+let accessAllowed = age > 18 ? true : false;
+alert(accessAllowed);*/
+
+/*let age = prompt("age?", 18);
+
+let message =
+  age < 3
+    ? "hi, baby"
+    : age < 18
+    ? "hello"
+    : age < 100
+    ? "Greetings"
+    : "What an unusual age";
+alert(message);*/
+
+//ejercicio 6 si
+
+/*let nombre = prompt("what is the official name of Javascript","");*/
+
+/*let numero = prompt("Introduce numero",0);
+    if (numero> 0){
+        alert(1);*/
+
+/*let result = a+b<4 ? Bellow:*/
+
+/*let message
+        login=="Employee"
+        ?"hello"
+        :login=="Director"
+        ?"Greetings"
+        :login==""
+        ?"No login"
+        :"";*/
