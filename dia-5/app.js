87c19d27f417c5f2c88cc3556adffca2b1c8e674
davidCarba <da.carballal@gmail.com>
2019-01-25 21:30:45 +0100

    Dia 5

--- dia-5/app.js
+++ dia-5/app.js
@@ -3,11 +3,109 @@
  * practicar Javascript!
  */
 
-console.log("Probando, probando, 1, 2, 3!");
+/*console.log("Probando, probando, 1, 2, 3!");*/
 
-function hoistExample() {
-  var a = 10;
+/*function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
 }
 
-hoistExample();
-console.log(a);
+function showOk() {
+  alert("You agreed.");
+}
+
+function showCancel() {
+  alert("You canceled the execution.");
+}
+
+// uso: las funciones showOk, showCancel se pasan como argumentos para preguntar
+ask("Do you agree?", showOk, showCancel);*/
+
+//ejercicio 1
+
+/*function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+    ask(
+    "Do you agree?",
+    function() { alert("You agreed."); },
+    function() { alert("You canceled the execution."); }
+  );
+
+  let ask = (question, yes, no)=> 
+    if (confirm(question))? yes()
+    :else no();
+  
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed,"),
+  () => alert("You cancelled the question.")
+);*/
+
+//ejercicio 2
+
+/*let user = {};
+user.name = "Manolo";
+user.surname = "Garcia";
+user.name = "NoMoreManolo";
+delete user.name;*/
+
+//ejercicio 3
+
+/*let schedule = {};
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    return true;
+  }
+  return false;
+}
+
+alert(isEmpty(schedule));
+
+schedule["8:30"] = "get up";*/
+
+//ejercicio 4
+
+/*const user = {
+    name: "John"
+  };
+  
+  // does it work?
+  user.name = "Pete";//si se puede cambiar lo unico que no 
+  //se puede cambiar es la referencia del objeto user*/
+
+//ejercicio 5
+
+/*let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+};
+
+let sum = 0;
+for (let key in salaries) {
+  sum += salaries[key];
+}
+alert(sum);*/
+
+//ejercicio 6
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof key !== String) {
+      obj[key] *= 2;
+    }
+  }
+}
+
+multiplyNumeric(menu);
+console.log(menu);
---
+++ dia-5/appExtra.js
@@ -0,0 +1,31 @@
+/*
+ * Aquí podéis hacer los ejercicios y
+ * practicar Javascript!
+ */
+
+/*console.log("Probando, probando, 1, 2, 3!");
+
+let user ={
+    edad: 21,
+    sexo:"Masculino",
+    name:"David",
+    surname:"Carballal"
+}
+
+function letterUpper(obj){
+    for(let key in obj){
+        if(obj[key]!== Number){
+            if(key.length>3){
+                for(i=0;i<key.length;i++){
+                    let input = key[i];
+                    let lower = input.toLowerCase();
+                    let firstletter = lower.slice(0,1);
+                    let capitalized =lower.replace(
+                        firstLetter,
+                        firstLetter.toUpperCase()
+                    );
+                }
+            }
+        }
+    }
+}*/
