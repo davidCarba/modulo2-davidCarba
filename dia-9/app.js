db2fbdf8ce0734030d976f94201fbddfa28a2c32
davidCarba <da.carballal@gmail.com>
2019-01-30 21:33:44 +0100

    Dia-9

---
+++ dia-9/app.js
@@ -0,0 +1,26 @@
+/*
+ * Aquí podéis hacer los ejercicios y
+ * practicar Javascript!
+ */
+
+//ejercicio 1
+
+/*function getMaxSubSum(arr) {
+  let finalSum = 0;
+  let temporal = 0;
+  for (let item of arr) {
+    // for each item of arr
+    temporal += item; // add it to partialSum
+    finalSum = Math.max(finalSum, temporal); // remember the maximum
+    if (temporal < 0) temporal = 0; // zero if negative
+  }
+
+  return finalSum;
+}
+
+alert(getMaxSubSum([-1, 2, 3, -9])); // 5
+alert(getMaxSubSum([-1, 2, 3, -9, 11])); // 11
+alert(getMaxSubSum([-2, -1, 1, 2])); // 3
+alert(getMaxSubSum([100, -9, 2, -3, 5])); // 100
+alert(getMaxSubSum([1, 2, 3])); // 6
+alert(getMaxSubSum([-1, -2, -3])); // 0*/
