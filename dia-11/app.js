0c83182e5acb85e8f40e6d1acda758a2fdd6019b
davidCarba <da.carballal@gmail.com>
2019-02-02 00:07:11 +0100

    repaso de la clase + ejercicios siguientes

--- dia-11/app.js
+++ dia-11/app.js
@@ -8,9 +8,13 @@
 /*function camelize(str) {
   return str
     .split("-")
-    .map((word, index) => index == 0 
-    ? word : word[0].toUpperCase() + word.slice(1)
-    )
+    .map(function check(word, index) {
+      if (index == 0) {
+        return word;
+      } else {
+        return word[0].toUpperCase() + word.slice(1);
+      }
+    })
     .join("");
 }
 
@@ -26,8 +30,6 @@ let result = arr.map(function(item, index, array) {
 result = result.join("");
 return result;
 }
-
-alert(camelize("cara-de-sapo"));
 
 alert(camelize("background-color"));
 alert(camelize("list-style-image"));
@@ -54,14 +56,96 @@ let filtered = filterRange(arr, 1, 4);
 
 alert(filtered); // 3,1 (matching values)
 
-alert(arr); // 5,3,8,1 (not modified)
-
-/*return arr.filter(item => (a <= item && item <= b));*/
+alert(arr); // 5,3,8,1 (not modified)*/
 
 //ejercicio 4
 
-/*let arr = [5, 2, 1, -10, 8];
+/*let arr = [5, -2, 1, -10, 8, 11];
 
-arr.sort((a, b) => b - a);
+//primera forma
+
+/*function reves(a, b) {
+  return b - a;
+}
+
+arr.sort(reves);
+
+//segunda forma
+
+/*arr.sort((a, b) => b - a);
 
 alert(arr);*/
+
+//ejercicio 5
+
+/*function copySorted(arr) {
+  let newArray = arr.slice(0);
+  newArray.sort();
+  return newArray;
+}
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert(sorted); // CSS, HTML, JavaScript
+alert(arr); // HTML, JavaScript, CSS (no changes)*/
+
+//ejercicio 6
+
+/*function Calculator() {
+  let calculo = {
+    "+": function(a, b) {
+      return a + b;
+    },
+    "-": function(a, b) {
+      return a - b;
+    }
+  };
+  this.calculate = function(str) {
+    let space = str.split(" "),
+      a = +space[0],
+      op = space[1],
+      b = +space[2];
+
+    if (!calculo[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
+    } else {
+      return calculo[op](a, b);
+    }
+  };
+  this.addMethod = function(name, func) {
+    calculo[name] = func;
+  };
+}
+
+let calc = new Calculator();
+
+alert(calc.calculate("3 + 7")); // 10
+
+let powerCalc = new Calculator();
+powerCalc.addMethod("*", function mult(a, b) {
+  return a * b;
+});
+powerCalc.addMethod("/", function div(a, b) {
+  return a / b;
+});
+powerCalc.addMethod("**", function pow(a, b) {
+  return a ** b;
+});
+
+let result = powerCalc.calculate("2 ** 3");
+alert(result); // 8*/
+
+//ejercicio 7
+
+/*let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [john, pete, mary];
+
+let names = users.map(function(users) {
+  return users.name;
+});
+
+alert(names); // John, Pete, Mary*/
